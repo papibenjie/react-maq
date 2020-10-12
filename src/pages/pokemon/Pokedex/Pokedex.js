853ce3badc763dc5d587a9010f96dc3a1cbd97b4
Marc-Antoine Quessy <papibenjie@gmail.com>
2020-10-12 01:17:06 -0400

    yrdy

--- src/pages/pokemon/Pokedex/Pokedex.js
+++ src/pages/pokemon/Pokedex/Pokedex.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 class Pokedex extends Component {
   render() {
-    return <div>editttt me: at components/Pokedex/Pokedex.js</div>
+    return <div>edit me: at components/Pokedex/Pokedex.js</div>
   }
 }
 
